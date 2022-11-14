fc405f8251966e0adbd3211f6f20ca6a1607922e
lioxryt <gsystemnotifs@gmail.com>
2022-11-14 12:23:31 -0500

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -15,6 +15,7 @@ say[7] = "This project was made possible due to extensive use of narcotics";
 say[8] = "Last chance to look at me Hector";
 say[9] = "👋hello👋 and welcome to the Los Pollos hermanos 👪family👪👪 my name📛 is Gustavo but you can 📲call 📲 me Gus";
 say[10] = "According to all known laws of aviation,  : there is no way a bee should be able to fly.";
+say[11] = "amojngus ahdahahahayah i scab cab  cab can spell correctky ahahahha your bad and im gododd";
 
 var howmany = say.length;
 var bRand = 0;
