43d6ef722763618c3074142fd15af7393a9297f9
lioxryt <gsystemnotifs@gmail.com>
2022-12-04 18:20:25 -0500

    llllllllllllllllllllllllllll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var gaenabled = window.localStorage.getItem("ga");
+if (gaenabled == "false") {
+  script("Skipped GA injection because it is disabled by the user.");
+} else {
+  const gascript = document.createElement("script");
+  gascript.setAttribute("async", "");
+  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
+  const inlinegascript = document.createElement("script");
+  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-98DP5VKS42');`;
+  document.head.append(gascript, inlinegascript);
+  script("Injected script 1/5");
+}
+
+const tabCloak = document.createElement("script");
+tabCloak.setAttribute("src", "/js/tab_cloak.js");
+document.head.append(tabCloak);
+script("Injected script 2/5");
+
 const arc = document.createElement("script");
 arc.setAttribute("async", "");
 arc.setAttribute("src", "https://arc.io/widget.min.js#eRPHFgiC");
