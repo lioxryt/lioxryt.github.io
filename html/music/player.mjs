ba88b2737d7035dd0ed583995b79066f3e21d0ca
kurb <119330240+kurbus@users.noreply.github.com>
2023-01-06 00:45:55 +0000

    awful coding !!!

--- html/music/player.mjs
+++ html/music/player.mjs
@@ -1,21 +1,24 @@
-import { Howl, Howler } from 'howler';
-import music from "./muslist.json";
+import {Howl, Howler} from './howler.js';
+import music from'./muslist.json' assert { type: 'json' };
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 var player= new Howl({
  src: music
 });
-
+console.log(music);
 function playMus() {
     player.stop();
     var indx=player.play(getRandomInt(music.length()));
+    console.log("Go")
 }
 
 function stopMus() {
     player.stop();
+    console.log('Stop')
 }
 function nextTrk() {
     player.stop();
     var indx2=player.play(indx+1)
+    console.log("Next")
 }
